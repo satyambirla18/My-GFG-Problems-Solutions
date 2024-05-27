c92f86c7734e78fea4bd2a0c63be331002af2700
Satyam Birla <159645371+satyambirla18@users.noreply.github.com>
2024-05-27 09:49:19 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  private:
+    int gcd(long long a, long long b){
+      if(b==0){
+          return a;
+      }else{
+          return gcd(b,a%b);
+      }
+    }
+
+   public:
+    vector<long long> lcmAndGcd(long long A , long long B){
+        // code here
+        vector<long long>res;
+        // long long a = lcm(A,B);
+        long long b = gcd(A,B);
+        long long a = A*B/b;
+        res.push_back(a);
+        res.push_back(b);
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long A,B;
+        
+        cin>>A>>B;
+
+        Solution ob;
+        vector<long long> ans = ob.lcmAndGcd(A,B);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
