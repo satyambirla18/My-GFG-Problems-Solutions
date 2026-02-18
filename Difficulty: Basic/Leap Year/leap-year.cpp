efc22fca3a19f27f1e42c3f3d69714f53888b1ad
Satyam Birla <159645371+satyambirla18@users.noreply.github.com>
2026-02-18 10:48:43 +0530

    \Optimised leap-year.cpp

--- Difficulty: Basic/Leap Year/leap-year.cpp
+++ Difficulty: Basic/Leap Year/leap-year.cpp
@@ -5,10 +5,7 @@ class Solution {
         if(n % 400 == 0){
             return true;
         }
-        else if (n % 100 == 0 && n % 4 == 0){
-            return false;
-        }
-        else if (n % 4 == 0){
+        else if (n % 4 == 0 && n % 100 != 0){
             return true;
         }
         else{
