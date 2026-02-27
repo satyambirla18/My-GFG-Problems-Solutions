6797650feb7705f5bae2ab6cab0b19744aaa4a39
Satyam Birla <159645371+satyambirla18@users.noreply.github.com>
2026-02-27 20:12:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Squares in N*N Chessboard/squares-in-nn-chessboard.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        long long int ans = 0;
+        while(N){
+            ans += N*N;
+            N--;
+        }
+        return ans;
+    }
+};
